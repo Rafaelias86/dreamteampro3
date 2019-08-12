dd1473861e6891436a617b3b67730a71e279ed87
amitsinghgh19 <prasadmanigh19>
2019-08-11 21:45:18 -0400

    added 404 gif if no match

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -4,7 +4,7 @@ function NoMatch() {
     return (
         <div className="notFound">
             <h1>404 Page Not Found</h1>
-            <img src="/assets/images/confused-travolta.gif" alt="confused Travolta" />
+            <img src="/assets/images/404giphy1.webp" alt="404error" />
         </div>
     );
 }
