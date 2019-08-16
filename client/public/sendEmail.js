c162dea60bb6fd9cb6313335bd32cb50781d9a4b
amitsinghgh19 <prasadmanigh19>
2019-08-16 14:53:26 -0400

    sendEmail update

--- client/public/sendEmail.js
+++ client/public/sendEmail.js
@@ -1,27 +1,27 @@
 const nodemailer = require('nodemailer');
 const crypt = require('../../models/crypt');
 const emailDB = require("../../models/Users");
-const encryptedPassword = '126891c03fef8f5fc1e1d8714d82bb';
+const encryptedPassword = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 var emailList = [];
 
 function pullEmails () {
-    emailList.push(emailDB.findmany({ users: (email) }))
+    emailList.push(emailDB.findmany({ users: ("email") }))
 }
 
 const transport = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-        user: 'SCKitchen1864@gmail.com',
+        user: 'ezvalet2019@gmail.com',
         pass: crypt.decrypt(encryptedPassword),
     },
 });
 module.exports = function sendEmail() {
     pullEmails();
     const mailOptions = {
-        from: 'SCKitchen1864@gmail.com',
+        from: 'ezvalet2019@gmail.com',
         to: emailList,
-        subject: "SC Kitchen has a new listing!"
-        html: message,
+        subject: "SC Kitchen has a new listing!",
+        html: "message",
     };
     transport.sendMail(mailOptions, (error) => {
         if (error) {
@@ -31,28 +31,3 @@ module.exports = function sendEmail() {
     });
 };
 
-//from W3 schools
-// var nodemailer = require('nodemailer');
-
-// var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'youremail@gmail.com',
-//     pass: 'yourpassword'
-//   }
-// });
-
-// var mailOptions = {
-//   from: 'youremail@gmail.com',
-//   to: 'myfriend@yahoo.com',
-//   subject: 'Sending Email using Node.js',
-//   text: 'That was easy!'
-// };
-
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// });
