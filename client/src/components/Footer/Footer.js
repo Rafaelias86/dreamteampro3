e3e309ca890b2aa70f2a2af8178bdd42ecf29021
amitsinghgh19 <prasadmanigh19>
2019-08-22 15:45:48 -0400

    pay-payment

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
             <button onClick={() => window.location='/notify'} type="button" class="btn btn-outline-success p-1 mx-1 mb-2 mt-2"><i class="fas fa-sms mx-2"></i>Notify</button>
             <button onClick={() => window.location='/scan'}type="button" class="btn btn-outline-light p-1 mx-1 mb-2 mt-2"><i class="fas fa-qrcode mx-2"></i>Scan</button>
             <button onClick={() => window.location='retrieve'}type="button" class="btn btn-outline-info p-1 mx-1 mb-2 mt-2"><i class="fas fa-search mx-2"></i>Retrieve</button>
-            <button onClick={() => window.location='/pay'}type="button" class="btn btn-outline-secondary p-1 mx-1 mb-2 mt-2"><i class="fas fa-dollar-sign mx-2"></i>Pay</button>
+            <button onClick={() => window.location='/pay'}type="button" class="btn btn-outline-secondary p-1 mx-1 mb-2 mt-2"><i class="fas fa-dollar-sign mx-2"></i>Payment</button>
             <button onClick={() => window.location='/space'}type="button" class="btn btn-outline-warning p-1 mx-1 mb-2 mt-2"><i class="fas fa-parking mx-2"></i>Space</button>
            
             {/* <p className="navbar-text my-2 my-lg-0">
